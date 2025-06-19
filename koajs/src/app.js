bbe703fdd4cfd8c435c85d7f558d7ba0258b494b
NhatPQ <nhatpq@avadagroup.com>
2025-06-19 21:58:38 +0700

    merge

--- koajs/src/app.js
+++ koajs/src/app.js
@@ -2,7 +2,17 @@ const Koa = require('koa');
 const koaBody = require('koa-body');
 const routes = require('./routes/routes.js');
 
+const render = require("koa-ejs");
+const path = require("path");
 const app = new Koa();
+
+render(app, {
+    root: path.join(__dirname, "views"),
+    layout: "layout/template",
+    viewExt: "html",
+    cache: false,
+    debug: true,
+});
 
 app.use(koaBody());
 app.use(routes.routes());
--- koajs/src/routes/routes.js
+++ koajs/src/routes/routes.js
@@ -2,16 +2,22 @@ const Router = require('koa-router');
 const productHandler = require('../handlers/products/productHandlers');
 const productInputMiddleware = require('../middleware/productInputMiddleware');
 const productUpdateMiddleware = require('../middleware/productUpdateMiddleware');
+const {getAll: getAllProducts} = require("../database/productRepository");
 
-const router = new Router({
-    prefix: '/api/products'
+const router = new Router();
+
+router.get('/products', async (ctx) => {
+    const products = getAllProducts();
+    await ctx.render('product', {
+        products
+    });
 });
 
-router.get('/', productHandler.getProducts);
-router.get('/:id', productHandler.getProduct);
-router.post('/', productInputMiddleware, productHandler.createProduct);
-router.put('/:id', productUpdateMiddleware, productHandler.updateProduct);
-router.delete('/:id', productHandler.deleteProduct);
+router.get('/api/products/', productHandler.getProducts);
+router.get('/api/products/:id', productHandler.getProduct);
+router.post('/api/products/', productInputMiddleware, productHandler.createProduct);
+router.put('/api/product/:id', productUpdateMiddleware, productHandler.updateProduct);
+router.delete('/api/product/:id', productHandler.deleteProduct);
 
 
 module.exports = router;
